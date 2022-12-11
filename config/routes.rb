f75fff6922fb6d6db9ee01aa49fdf4771db98838
Felix Ondullah <felixondullah@gmail.com>
2022-12-11 18:18:02 +0300

    added seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
-resources :admins
-#  resources :users
-
-get '/signup', to: 'users#new'
-post '/signup', to: 'users#create'
-get '/me', to: 'users#show'
-post '/login', to: 'sessions#create'
-delete '/logout', to: 'sessions#destroy'
-# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-# Defines the root path route ("/")
-# root "articles#index"
-resources :orders
+Rails.application.routes.draw do
+    resources :admins
+    #  resources :users
+  
+    get '/signup', to: 'users#new'
+    post '/signup', to: 'users#create'
+    get '/me', to: 'users#show'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Defines the root path route ("/")
+    # root "articles#index"
+    resources :orders
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,22 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+
+
+
+puts "🐕 Seeding data..."
+
+User.create(username: "Luke", email:"luke@gmail.com",     password:"lukasam", password_confirmation:"lukasam")
+User.create(username: "mahrez", email:"mahrez@gmail.com",   password:"mahrezali", password_confirmation:"mahrezali")
+User.create(username: "dembele", email:"dembele@gmail.com",  password:"dembelee", password_confirmation:"dembelee")
+User.create(username: "dele",  email:"dele@gmail.com",    password:"deleali", password_confirmation:"deleali")
+User.create(username: "maahir", email:"maahir@gmail.com",   password:"mahrezy", password_confirmation:"mahrezy")
+
+
+puts "✅ Done seeding!"
 #   Character.create(name: "Luke", movie: movies.first)
+
+order1=Order.create(package_type:"glass", pick_up_location:"Nairobi", drop_off_location:"Eldoret" ,date:"10/12/2022" , receiver_name:"Sugut Anne", number_of_kgs:"2")
+order2=Order.create(package_type:"electronic", pick_up_location:"London", drop_off_location:"Busia", date:"5/12/2022", receiver_name:"Okoth Alex",number_of_kgs:"10")
+order3=Order.create(package_type:"plastics", pick_up_location:"Kinoo", drop_off_location:"Kisumu", date:"23/12/2022", receiver_name:"Moha Ali", number_of_kgs:"30")
+order4=Order.create(package_type:"boxes", pick_up_location:"Uthiru" ,drop_off_location:"Kigali", date:"1/1/2022", receiver_name:"Wanyama Victor" ,number_of_kgs:"28")
