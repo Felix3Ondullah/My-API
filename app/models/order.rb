55d0701d056009914e8af4745d0d26245f015550
Felix Ondullah <felixondullah@gmail.com>
2022-12-11 18:04:23 +0300

    added attributes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ApplicationRecord
+    validates :package_type,:pick_up_location, :drop_off_location, :date, :receiver_name, :number_of_kgs, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :password, :presence => true,:length => {:within => 6..30},  :on => :create
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
+   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+   # :confirmation => true,
+  
+ # has_many :orders
+
 end
